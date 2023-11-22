7aa03b7f16bcdedc4505589bc2b2527e74f16c84
Jerald Austero <jerald.austero@tawk.to>
2023-11-22 23:44:18 +0800

    Add storybook documentation for badge

---
+++ src/stories/IBadge.stories.ts
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+import IBadge from '@/components/badge/IBadge.vue';
+
+const meta: Meta<typeof IBadge> = {
+	title: 'Atoms/Badge',
+	parameters: {
+		docs: {
+			description: {
+				component: 'Easily create nice looking notification badges.',
+			},
+		},
+	},
+	component: IBadge,
+	tags: ['autodocs'],
+	argTypes: {
+		count: {
+			control: {
+				type: 'number',
+				min: 0,
+			},
+		},
+	},
+	args: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof IBadge>;
+
+export const DefaultBadge: Story = {
+	render: (args) => ({
+		components: { IBadge },
+		setup() {
+			return { args };
+		},
+		template: `<i-badge v-bind="args"/>`,
+	}),
+
+	args: {
+		count: 12,
+	},
+};
