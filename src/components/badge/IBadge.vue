0d257d8843bc8e125986d5fa32d661cc3fd88bc4
Jerald Austero <jerald.austero@tawk.to>
2023-11-22 23:44:11 +0800

    Add badge component

---
+++ src/components/badge/IBadge.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps<{
+	/**
+	 * A number that will show in the badge
+	 */
+	count: number;
+}>();
+</script>
+
+<template>
+	<span class="i-badge">
+		{{ props.count }}
+	</span>
+</template>
