9c31eaa3c936a95546d1226de981fe7f590bea88
Jerald Austero <jerald.austero@tawk.to>
2023-11-29 23:45:13 +0800

    Fix indention; Import global style

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,20 @@
-import type { Preview } from '@storybook/vue3'
+import type { Preview } from '@storybook/vue3';
+
+/**
+ * Global style
+ */
+import '@/scss/styles.scss';
 
 const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i
-      }
-    }
-  }
-}
+	parameters: {
+		actions: { argTypesRegex: '^on[A-Z].*' },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
+};
 
-export default preview
+export default preview;
