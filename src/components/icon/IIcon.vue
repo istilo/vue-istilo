605f307a51ed4ca989134198fd7740aed565cf5e
Jerald Austero <jerald.austero@tawk.to>
2023-12-17 13:21:39 +0800

    Add icon component

---
+++ src/components/icon/IIcon.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{
+	/**
+	 * A size modifier that increase or decrease the font-size
+	 * @values 'xsmall', 'small', 'medium', large', 'xlarge', 'xxlarge'
+	 */
+	size?: string;
+
+	/**
+	 * Specify the type of the icon based on fontello of tawkto icon set
+	 */
+	type: string;
+}>();
+
+const getClasses = computed(() => {
+	return [
+		'i-icon',
+		props.type ? `i-icon-${props.type}` : '',
+		props.size ? `i-icon-${props.size}` : '',
+	];
+});
+</script>
+
+<template>
+	<i :class="getClasses" />
+</template>
