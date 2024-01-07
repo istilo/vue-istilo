eea5306e8b0098fa80a467809c806dee1ec95e9f
Jerald Austero <jerald.austero@tawk.to>
2024-01-07 12:48:22 +0800

    Add label component

---
+++ src/components/label/ILabel.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{
+	/**
+	 * A color modifier for label
+	 */
+	color: string;
+}>();
+
+const getClasses = computed(() => {
+	return ['i-label', props.color ? `i-label-${props.color}` : ''];
+});
+</script>
+
+<template>
+	<div :class="getClasses">
+		<slot />
+	</div>
+</template>
