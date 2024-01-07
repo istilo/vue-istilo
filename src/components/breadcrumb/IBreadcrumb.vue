4a6a3716d9b3811a0937d628a984d615092d6c8c
Jerald Austero <jerald.austero@tawk.to>
2024-01-07 12:48:49 +0800

    Add breadcrumb component

---
+++ src/components/breadcrumb/IBreadcrumb.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import IBreadcrumbItem from './IBreadcrumbItem.vue';
+
+interface Items {
+	href: string;
+	isDisabled: boolean;
+	label: string;
+}
+
+const props = defineProps<{
+	/**
+	 * An array of object to display link
+	 *
+	 * @example { href : string, isDisabled : boolean, label : string }
+	 */
+	items: Items[];
+}>();
+
+const isLastItem = computed(() => {
+	return props.items.length - 1;
+});
+</script>
+
+<template>
+	<ul class="i-breadcrumb">
+		<i-breadcrumb-item
+			v-for="(item, index) in items"
+			:key="index"
+			:isActive="isLastItem === index"
+			:href="item.href"
+			:isDisabled="item.isDisabled">
+			{{ item.label }}
+		</i-breadcrumb-item>
+	</ul>
+</template>
---
+++ src/components/breadcrumb/IBreadcrumbItem.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{
+	/**
+	 * Identifier for current page
+	 */
+	isActive?: boolean;
+
+	/**
+	 * Disabled the click event
+	 */
+	isDisabled?: boolean;
+
+	/**
+	 * Location where to redirect
+	 */
+	href: string;
+}>();
+
+const getClasses = computed(() => {
+	return [props.isDisabled ? 'i-disabled' : ''];
+});
+
+const getTagName = computed(() => {
+	return props.isActive ? 'span' : 'router-link';
+});
+</script>
+
+<template>
+	<li :class="getClasses">
+		<component
+			:is="getTagName"
+			:to="!props.isActive && props.href ? 'props.href' : ''">
+			<slot />
+		</component>
+	</li>
+</template>
